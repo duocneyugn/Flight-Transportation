cdd3acc9a2aa24609514bad140f499aedf5f4139
Duoc Nguyen <duocnguyen1996@gmail.com>
2016-11-12 15:55:56 -0800

    updated version

--- flight_database.sql
+++ flight_database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 12, 2016 at 11:58 PM
+-- Generation Time: Nov 13, 2016 at 12:53 AM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 7.0.9
 
@@ -163,6 +163,56 @@ CREATE TABLE `flight1` (
   `arrivalTime` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `flight1`
+--
+
+INSERT INTO `flight1` (`flightNumber`, `duration`, `departureDay`, `arrivalDay`, `departureTime`, `arrivalTime`) VALUES
+(1000, 1, 'Monday', 'Monday', '13:00', '14:00'),
+(1001, 1, 'Monday', 'Monday', '13:30', '14:30'),
+(1002, 1, 'Monday', 'Monday', '14:00', '15:00'),
+(1003, 1, 'Monday', 'Monday', '14:30', '15:30'),
+(1004, 1, 'Monday', 'Monday', '14:00', '15:00'),
+(1005, 1, 'Monday', 'Monday', '14:30', '15:30'),
+(1006, 1, 'Monday', 'Monday', '15:00', '16:00'),
+(1007, 1, 'Monday', 'Monday', '15:30', '16:30'),
+(1008, 1, 'Tuesday', 'Tuesday', '13:00', '14:00'),
+(1009, 1, 'Tuesday', 'Tuesday', '13:30', '14:30'),
+(1010, 1, 'Tuesday', 'Tuesday', '14:00', '15:00'),
+(1011, 1, 'Tuesday', 'Tuesday', '14:30', '15:30'),
+(1012, 1, 'Tuesday', 'Tuesday', '15:00', '16:00'),
+(1013, 1, 'Tuesday', 'Tuesday', '14:00', '15:00'),
+(1014, 1, 'Tuesday', 'Tuesday', '14:30', '15:30'),
+(1015, 1, 'Tuesday', 'Tuesday', '15:00', '16:00'),
+(1016, 1, 'Tuesday', 'Tuesday', '15:30', '16:30'),
+(1017, 1, 'Tuesday', 'Tuesday', '16:00', '17:00'),
+(1018, 1, 'Wednesday', 'Wednesday', '13:00', '14:00'),
+(1019, 1, 'Wednesday', 'Wednesday', '13:30', '14:30'),
+(1020, 1, 'Wednesday', 'Wednesday', '14:00', '15:00'),
+(1021, 1, 'Wednesday', 'Wednesday', '14:30', '15:30'),
+(1022, 1, 'Wednesday', 'Wednesday', '14:00', '15:00'),
+(1023, 1, 'Wednesday', 'Wednesday', '14:30', '15:30'),
+(1024, 1, 'Wednesday', 'Wednesday', '15:00', '16:00'),
+(1025, 1, 'Wednesday', 'Wednesday', '15:30', '16:30'),
+(1026, 1, 'Thursday', 'Thursday', '13:00', '14:00'),
+(1027, 1, 'Thursday', 'Thursday', '13:30', '14:30'),
+(1028, 1, 'Thursday', 'Thursday', '14:00', '15:00'),
+(1029, 1, 'Thursday', 'Thursday', '14:30', '15:30'),
+(1030, 1, 'Thursday', 'Thursday', '15:00', '16:00'),
+(1031, 1, 'Thursday', 'Thursday', '14:00', '15:00'),
+(1032, 1, 'Thursday', 'Thursday', '14:30', '15:30'),
+(1033, 1, 'Thursday', 'Thursday', '15:00', '16:00'),
+(1034, 1, 'Thursday', 'Thursday', '15:30', '16:30'),
+(1035, 1, 'Thursday', 'Thursday', '16:00', '17:00'),
+(1036, 1, 'Friday', 'Friday', '13:00', '14:00'),
+(1037, 1, 'Friday', 'Friday', '13:30', '14:30'),
+(1038, 1, 'Friday', 'Friday', '14:00', '15:00'),
+(1039, 1, 'Friday', 'Friday', '14:30', '15:30'),
+(1040, 1, 'Friday', 'Friday', '14:00', '15:00'),
+(1041, 1, 'Friday', 'Friday', '14:30', '15:30'),
+(1042, 1, 'Friday', 'Friday', '15:00', '16:00'),
+(1043, 1, 'Friday', 'Friday', '15:30', '16:30');
+
 -- --------------------------------------------------------
 
 --
@@ -175,6 +225,56 @@ CREATE TABLE `flight2` (
   `arrivingAirport` varchar(100) NOT NULL,
   `departingAirport` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `flight2`
+--
+
+INSERT INTO `flight2` (`flightNumber`, `modelNumber`, `arrivingAirport`, `departingAirport`) VALUES
+(1000, 707, 'San Jose State University', 'Cal Maritime'),
+(1001, 708, 'San Jose State University', 'Cal Poly Pomona'),
+(1002, 709, 'San Jose State University', 'Cal Poly San Luis Obispo'),
+(1003, 710, 'San Jose State University', 'CSU Bakersfield'),
+(1004, 707, 'Cal Maritime', 'San Jose State University'),
+(1005, 708, 'Cal Poly Pomona', 'San Jose State University'),
+(1006, 709, 'Cal Poly San Luis Obispo', 'San Jose State University'),
+(1007, 710, 'CSU Bakersfield', 'San Jose State University'),
+(1008, 711, 'San Jose State University', 'CSU Channel Islands'),
+(1009, 713, 'San Jose State University', 'CSU Chico'),
+(1010, 714, 'San Jose State University', 'CSU Dominguez Hills'),
+(1011, 715, 'San Jose State University', 'CSU East Bay'),
+(1012, 716, 'San Jose State University', 'CSU Fresno'),
+(1013, 711, 'CSU Channel Islands', 'San Jose State University'),
+(1014, 713, 'CSU Chico', 'San Jose State University'),
+(1015, 714, 'CSU Dominguez Hills', 'San Jose State University'),
+(1016, 715, 'CSU East Bay', 'San Jose State University'),
+(1017, 716, 'CSU Fresno', 'San Jose State University'),
+(1018, 717, 'San Jose State University', 'CSU Fullerton'),
+(1019, 718, 'San Jose State University', 'CSU Long Beach'),
+(1020, 719, 'San Jose State University', 'CSU Los Angeles'),
+(1021, 720, 'San Jose State University', 'CSU Monterey Bay'),
+(1022, 717, 'CSU Fullerton', 'San Jose State University'),
+(1023, 718, 'CSU Long Beach', 'San Jose State University'),
+(1024, 719, 'CSU Los Angeles', 'San Jose State University'),
+(1025, 720, 'CSU Monterey Bay', 'San Jose State University'),
+(1026, 721, 'San Jose State University', 'CSU Northridge'),
+(1027, 722, 'San Jose State University', 'CSU Sacramento'),
+(1028, 723, 'San Jose State University', 'CSU San Bernardino'),
+(1029, 724, 'San Jose State University', 'CSU San Marcos'),
+(1030, 725, 'San Jose State University', 'CSU Stanislaus'),
+(1031, 721, 'CSU Northridge', 'San Jose State University'),
+(1032, 722, 'CSU Sacramento', 'San Jose State University'),
+(1033, 723, 'CSU San Bernardino', 'San Jose State University'),
+(1034, 724, 'CSU San Marcos', 'San Jose State University'),
+(1035, 725, 'CSU Stanislaus', 'San Jose State University'),
+(1036, 726, 'San Jose State University', 'Humboldt State University'),
+(1037, 727, 'San Jose State University', 'San Diego State University'),
+(1038, 728, 'San Jose State University', 'San Francisco State University'),
+(1039, 729, 'San Jose State University', 'Sonoma State University'),
+(1040, 726, 'Humboldt State University', 'San Jose State University'),
+(1041, 727, 'San Diego State University', 'San Jose State University'),
+(1042, 728, 'San Francisco State University', 'San Jose State University'),
+(1043, 729, 'Sonoma State University', 'San Jose State University');
 
 -- --------------------------------------------------------
 
@@ -368,9 +468,89 @@ INSERT INTO `passenger` (`passengerID`, `passportNo`, `name`) VALUES
 
 CREATE TABLE `seatbooking1` (
   `seat` varchar(3) NOT NULL,
-  `seatPreference` varchar(2) NOT NULL,
+  `seatPreference` varchar(10) NOT NULL,
   `serviceClass` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `seatbooking1`
+--
+
+INSERT INTO `seatbooking1` (`seat`, `seatPreference`, `serviceClass`) VALUES
+('10A', 'Window', 'Economy'),
+('10B', 'Center', 'Economy'),
+('10C', 'Aisle', 'Economy'),
+('10D', 'Aisle', 'Economy'),
+('10E', 'Center', 'Economy'),
+('10F', 'Window', 'Economy'),
+('11A', 'Window', 'Economy'),
+('11B', 'Center', 'Economy'),
+('11C', 'Aisle', 'Economy'),
+('11D', 'Aisle', 'Economy'),
+('11E', 'Center', 'Economy'),
+('11F', 'Window', 'Economy'),
+('12A', 'Window', 'Economy'),
+('12B', 'Center', 'Economy'),
+('12C', 'Aisle', 'Economy'),
+('12D', 'Aisle', 'Economy'),
+('12E', 'Center', 'Economy'),
+('12F', 'Window', 'Economy'),
+('13A', 'Window', 'Economy'),
+('13B', 'Center', 'Economy'),
+('13C', 'Aisle', 'Economy'),
+('13D', 'Aisle', 'Economy'),
+('13E', 'Center', 'Economy'),
+('13F', 'Window', 'Economy'),
+('14A', 'Window', 'Economy'),
+('14B', 'Center', 'Economy'),
+('14C', 'Aisle', 'Economy'),
+('14D', 'Aisle', 'Economy'),
+('14E', 'Center', 'Economy'),
+('14F', 'Window', 'Economy'),
+('15A', 'Window', 'Economy'),
+('15B', 'Center', 'Economy'),
+('15C', 'Aisle', 'Economy'),
+('15D', 'Aisle', 'Economy'),
+('15E', 'Center', 'Economy'),
+('15F', 'Window', 'Economy'),
+('16A', 'Window', 'Economy'),
+('16B', 'Center', 'Economy'),
+('16C', 'Aisle', 'Economy'),
+('16D', 'Aisle', 'Economy'),
+('16E', 'Center', 'Economy'),
+('16F', 'Window', 'Economy'),
+('17A', 'Window', 'Economy'),
+('17B', 'Center', 'Economy'),
+('17C', 'Aisle', 'Economy'),
+('17D', 'Aisle', 'Economy'),
+('17E', 'Center', 'Economy'),
+('17F', 'Window', 'Economy'),
+('18A', 'Window', 'Economy'),
+('18B', 'Center', 'Economy'),
+('18C', 'Aisle', 'Economy'),
+('18D', 'Aisle', 'Economy'),
+('18E', 'Center', 'Economy'),
+('18F', 'Window', 'Economy'),
+('19A', 'Window', 'Economy'),
+('19B', 'Center', 'Economy'),
+('19C', 'Aisle', 'Economy'),
+('19D', 'Aisle', 'Economy'),
+('19E', 'Center', 'Economy'),
+('19F', 'Window', 'Economy'),
+('1A', 'Window', 'First'),
+('1B', 'Aisle', 'First'),
+('1C', 'Aisle', 'First'),
+('1D', 'Window', 'First'),
+('20A', 'Window', 'Economy'),
+('20B', 'Center', 'Economy'),
+('20C', 'Aisle', 'Economy'),
+('20D', 'Aisle', 'Economy'),
+('20E', 'Center', 'Economy'),
+('20F', 'Window', 'Economy'),
+('2A', 'Window', 'First'),
+('2B', 'Aisle', 'First'),
+('2C', 'Aisle', 'First'),
+('2D', 'Window', 'First');
 
 -- --------------------------------------------------------
 
